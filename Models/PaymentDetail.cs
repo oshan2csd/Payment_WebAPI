a6f2a017cf680a4c60d343cefddd6ffed44f0146
Oshan <oshan2csd@gmail.com>
2019-02-25 20:08:24 +1100

    initial commit with PaymentDetail model class

---
+++ Models/PaymentDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payment_WebAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PMId { get; set; }
+
+        [Required]
+        public string CardOwnerName{ get; set; }
+
+        [Required]
+        public string CardNumber { get; set; }
+
+        [Required]
+        public string ExpiryDate { get; set; }
+
+        [Required]
+        public string CVV { get; set; }
+
+
+    }
+}
