613bed791ba4aca3742954d397fd9472e7329bb1
Oshan <oshan2csd@gmail.com>
2019-02-26 19:00:16 +1100

    paymentDetail and context classes

--- Models/PaymentDetail.cs
+++ Models/PaymentDetail.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,15 +13,19 @@ namespace Payment_WebAPI.Models
         public int PMId { get; set; }
 
         [Required]
+        [Column(TypeName = "nvarchar(100)")]//limiting data space required in sql db. By defalt it's nvarchar(max)
         public string CardOwnerName{ get; set; }
 
         [Required]
+        [Column(TypeName = "varchar(16)")]
         public string CardNumber { get; set; }
 
         [Required]
+        [Column(TypeName ="varchar(5)")] // MM/YY
         public string ExpiryDate { get; set; }
 
         [Required]
+        [Column(TypeName = "varchar(3)")]
         public string CVV { get; set; }
 
 
--- Models/PaymentDetailContext.cs
+++ Models/PaymentDetailContext.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Payment_WebAPI.Models
 {
-    public class PaymentDetailContext
+    public class PaymentDetailContext:DbContext
     {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
+        {
+        }
+
+        public DbSet<PaymentDetail> paymentDetails { get; set; }
     }
 }
